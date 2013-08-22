29f217dd0c7293fb42d94112d3459a61beb6debf
h2kyeong <h2kyeong@users.noreply.github.com>
2013-08-22 14:16:43 +0900

    Update raw.c
    renamed str- somethings to bin- somethings
    added binary to hex dump and backwards

--- raw.c
+++ raw.c
@@ -1,6 +1,7 @@
 // input and output binary numbers
 // h2kyeong, 2013. 8. 13
 
+#define PY_SSIZE_T_CLEAN  // must preceed Python.h
 #include <Python.h>
 #define PyDEF  static PyObject*
 #define PyARGS  PyObject *self, PyObject *args
@@ -70,7 +71,7 @@ PyDEF ftell_ (PyARGS){
 PyDEF fwrite_ (PyARGS){
 	int ok;
 	const char* val;
-	unsigned int len;
+	Py_ssize_t len;
 	FILE* fout = stdout;
 	ok = PyArg_ParseTuple(args, "s#|l", &val, &len, &fout);
 	if (!ok) return 0;
@@ -160,7 +161,7 @@ PyDEF write_double (PyARGS){
 
 
 
-PyDEF str_long (PyARGS){
+PyDEF bin_long (PyARGS){
 	int ok;
 	long val;
 	ok = PyArg_ParseTuple(args, "l", &val);
@@ -168,7 +169,7 @@ PyDEF str_long (PyARGS){
 	return Py_BuildValue("s#", &val, sizeof(long));
 }
 
-PyDEF str_int (PyARGS){
+PyDEF bin_int (PyARGS){
 	int ok;
 	int val;
 	ok = PyArg_ParseTuple(args, "i", &val);
@@ -176,7 +177,7 @@ PyDEF str_int (PyARGS){
 	return Py_BuildValue("s#", &val, sizeof(int));
 }
 
-PyDEF str_short (PyARGS){
+PyDEF bin_short (PyARGS){
 	int ok;
 	short val;
 	ok = PyArg_ParseTuple(args, "h", &val);
@@ -184,7 +185,7 @@ PyDEF str_short (PyARGS){
 	return Py_BuildValue("s#", &val, sizeof(short));
 }
 
-PyDEF str_ulong (PyARGS){
+PyDEF bin_ulong (PyARGS){
 	int ok;
 	unsigned long val;
 	ok = PyArg_ParseTuple(args, "k", &val);
@@ -192,7 +193,7 @@ PyDEF str_ulong (PyARGS){
 	return Py_BuildValue("s#", &val, sizeof(unsigned long));
 }
 
-PyDEF str_uint (PyARGS){
+PyDEF bin_uint (PyARGS){
 	int ok;
 	unsigned int val;
 	ok = PyArg_ParseTuple(args, "I", &val);
@@ -200,7 +201,7 @@ PyDEF str_uint (PyARGS){
 	return Py_BuildValue("s#", &val, sizeof(unsigned int));
 }
 
-PyDEF str_ushort (PyARGS){
+PyDEF bin_ushort (PyARGS){
 	int ok;
 	unsigned short val;
 	ok = PyArg_ParseTuple(args, "H", &val);
@@ -208,7 +209,7 @@ PyDEF str_ushort (PyARGS){
 	return Py_BuildValue("s#", &val, sizeof(unsigned short));
 }
 
-PyDEF str_float (PyARGS){
+PyDEF bin_float (PyARGS){
 	int ok;
 	float val;
 	ok = PyArg_ParseTuple(args, "f", &val);
@@ -216,12 +217,95 @@ PyDEF str_float (PyARGS){
 	return Py_BuildValue("s#", &val, sizeof(float));
 }
 
-PyDEF str_double (PyARGS){
+PyDEF bin_double (PyARGS){
 	int ok;
 	double val;
 	ok = PyArg_ParseTuple(args, "d", &val);
 	if (!ok) return 0;
 	return Py_BuildValue("s#", &val, sizeof(double));
+}
+
+char const CHARSET[] = "0123456789abcdef";
+
+PyDEF bin2hex (PyARGS){
+	int ok;
+	const char* val;
+	Py_ssize_t len;
+	ok = PyArg_ParseTuple(args, "s#", &val, &len);
+	if (!ok) return 0;
+	
+	char* out = (char*) calloc(sizeof(char), len*2);
+	if (!out){
+		PyErr_SetString(PyExc_MemoryError, "failed to allocate memory for output");
+		return 0;
+	}
+	// WARNING FREE MEMORY BEFORE RETURN
+	
+	const char* ival = val;
+	char* iout = out;
+	for (Py_ssize_t i=0; i<len; ++i){
+		int c = *ival;
+		*iout = CHARSET[(c & 240) >> 4]; ++iout;
+		*iout = CHARSET[c & 15]; ++iout;
+		++ival;
+	}
+	
+	PyObject* ret = Py_BuildValue("s#", out, sizeof(char) * len*2);
+	free(out);
+	return ret;
+}
+
+// returns 0 to 15
+static int __hex2bin_char (char c){
+	if ('0' <= c && c <= '9') return (c - '0');
+	if ('a' <= c && c <= 'f') return (c - 'a' + 10);
+	if ('A' <= c && c <= 'F') return (c - 'A' + 10);
+	return -1;
+}
+
+PyDEF hex2bin (PyARGS){
+	int ok;
+	const char* val;
+	Py_ssize_t len;
+	ok = PyArg_ParseTuple(args, "s#", &val, &len);
+	if (!ok) return 0;
+	if (len & 1){
+		PyErr_SetString(PyExc_Exception, "length is not even");
+		return 0;
+	}
+	
+	unsigned char* out = (unsigned char*) calloc(sizeof(char), len/2);
+	if (!out){
+		PyErr_SetString(PyExc_MemoryError, "failed to allocate memory for output");
+		return 0;
+	}
+	// WARNING FREE MEMORY BEFORE RETURN
+	
+	unsigned char* iout = out;
+	for (Py_ssize_t i=0; i<len; i+=2){
+		int v;
+		const char* c = val + i;
+		ok = __hex2bin_char(c[1]);
+		if (ok < 0){
+			PyErr_SetString(PyExc_Exception, "illegal character");
+			free(out);
+			return 0;
+		}
+		v = ok;
+		ok = __hex2bin_char(c[0]);
+		if (ok < 0){
+			PyErr_SetString(PyExc_Exception, "illegal character");
+			free(out);
+			return 0;
+		}
+		v += ok << 4;
+		*iout = v;
+		++iout;
+	}
+	
+	PyObject* ret = Py_BuildValue("s#", out, sizeof(char) * len/2);
+	free(out);
+	return ret;
 }
 
 static PyMethodDef methods[] = {
@@ -230,22 +314,24 @@ static PyMethodDef methods[] = {
 	{"write", fwrite_, METH_VARARGS, "write(str, addr=stdout)\n" "write a string"},
 	{"seek", fseek_, METH_VARARGS, "seek(addr, pos, mode=SEEK_SET)\n" "go to byte offset of current file"},
 	{"tell", ftell_, METH_VARARGS, "tell(addr)\n" "get the byte offset of current file"},
-	{"write_i8", write_long, METH_VARARGS, "write(val, addr=stdout)\n" "write a signed long in binary"},
-	{"write_i4", write_int, METH_VARARGS, "write(val, addr=stdout)\n" "write a signed int in binary"},
-	{"write_i2", write_short, METH_VARARGS, "write(val, addr=stdout)\n" "write a signed short in binary"},
-	{"write_u8", write_ulong, METH_VARARGS, "write(val, addr=stdout)\n" "write an unsigned long in binary"},
-	{"write_u4", write_uint, METH_VARARGS, "write(val, addr=stdout)\n" "write an unsigned int in binary"},
-	{"write_u2", write_ushort, METH_VARARGS, "write(val, addr=stdout)\n" "write an unsigned short in binary"},
-	{"write_f8", write_double, METH_VARARGS, "write(val, addr=stdout)\n" "write a signed double in binary"},
-	{"write_f4", write_float, METH_VARARGS, "write(val, addr=stdout)\n" "write a signed float in binary"},
-	{"str_i8", str_long, METH_VARARGS, "binary representation of a signed long"},
-	{"str_i4", str_int, METH_VARARGS, "binary representation of a signed integer"},
-	{"str_i2", str_short, METH_VARARGS, "binary representation of a signed short"},
-	{"str_u8", str_ulong, METH_VARARGS, "binary representation of an unsigned long"},
-	{"str_u4", str_uint, METH_VARARGS, "binary representation of an unsigned integer"},
-	{"str_u2", str_ushort, METH_VARARGS, "binary representation of an unsigned short"},
-	{"str_f8", str_double, METH_VARARGS, "binary representation of a double"},
-	{"str_f4", str_float, METH_VARARGS, "binary representation of a float"},
+	{"write_f8", write_double, METH_VARARGS, "write(float, addr=stdout)\n" "write a signed double in binary"},
+	{"write_f4", write_float, METH_VARARGS, "write(float, addr=stdout)\n" "write a signed float in binary"},
+	{"write_i8", write_long, METH_VARARGS, "write(int, addr=stdout)\n" "write a signed long in binary"},
+	{"write_i4", write_int, METH_VARARGS, "write(int, addr=stdout)\n" "write a signed int in binary"},
+	{"write_i2", write_short, METH_VARARGS, "write(int, addr=stdout)\n" "write a signed short in binary"},
+	{"write_u8", write_ulong, METH_VARARGS, "write(int, addr=stdout)\n" "write an unsigned long in binary"},
+	{"write_u4", write_uint, METH_VARARGS, "write(int, addr=stdout)\n" "write an unsigned int in binary"},
+	{"write_u2", write_ushort, METH_VARARGS, "write(int, addr=stdout)\n" "write an unsigned short in binary"},
+	{"bin_i8", bin_long, METH_VARARGS, "binary representation of a signed long"},
+	{"bin_i4", bin_int, METH_VARARGS, "binary representation of a signed integer"},
+	{"bin_i2", bin_short, METH_VARARGS, "binary representation of a signed short"},
+	{"bin_u8", bin_ulong, METH_VARARGS, "binary representation of an unsigned long"},
+	{"bin_u4", bin_uint, METH_VARARGS, "binary representation of an unsigned integer"},
+	{"bin_u2", bin_ushort, METH_VARARGS, "binary representation of an unsigned short"},
+	{"bin_f8", bin_double, METH_VARARGS, "binary representation of a double"},
+	{"bin_f4", bin_float, METH_VARARGS, "binary representation of a float"},
+	{"hex2bin", hex2bin, METH_VARARGS, "hex dump into a binary string"},
+	{"bin2hex", bin2hex, METH_VARARGS, "convert a hex dump to a binary string"},
 	{0,0,0,0}
 };
 
